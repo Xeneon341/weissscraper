9919a6e6c35503a88256dc7684e01deb83b2ddaf
Xeneon341 <56289476+Xeneon341@users.noreply.github.com>
2021-04-13 10:38:10 -0700

    consolidated functions under a single function. That way, it will be easier to call the function when the time comes.

--- weiss_combiner.py
+++ weiss_combiner.py
@@ -1,38 +1,38 @@
 import os, re, xlrd, openpyxl
 
 path = r"C:\Users\Alex Thompson\Barbaras Development Inc\NP - Documents\NP-Share\Weiss"
-filelist = []
-container_list = []
-filtered_filelist = []
 
 def extract_containers(any_list):
-    loc = (os.path.join(path, "Templates\container_config.xls"))
+
+
+    return any_list
+
+
+def extract_weiss_files(directory):
+    filelist = []
+    container_list = []
+    filtered_filelist = []
+
+    loc = (os.path.join(directory, "Templates\container_config.xls"))
 
     wb = xlrd.open_workbook(loc)
     sheet = wb.sheet_by_index(0)
     sheet.cell_value(0, 0)
 
     for i in range(sheet.nrows):
-        any_list.append(sheet.cell_value(i, 0))
+        container_list.append(sheet.cell_value(i, 0))
 
-    return any_list
-
-
-def extract_weiss_files(directory, any_list):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            any_list.append(os.path.join(root,file))
+            filelist.append(os.path.join(root,file))
 
-    return any_list
-
-def filter_files(string, substr):
-    for str in string:
-        for sub in substr:
+    for str in filelist:
+        for sub in container_list:
             if sub in str:
                 filtered_filelist.append(str)
-    # filtered_filelist.append(str for str in string if
-    #                         any(sub in str for sub in substr))
+                # filtered_filelist.append(str for str in string if
+                #                         any(sub in str for sub in substr))
 
     return filtered_filelist
 
-print(filter_files(extract_weiss_files(path, filelist), extract_containers(container_list)))
+print(extract_weiss_files(path))
