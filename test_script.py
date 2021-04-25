b2db4c8df6fc49abab35cf672ac770af96402bfa
Xeneon341 <56289476+Xeneon341@users.noreply.github.com>
2021-04-24 19:30:18 -0700

    was able to get camelot to work once I changed the flavor of the parse to stream instead of lattice. This may be able to work for me if I can parse the files using regex. Excel files will be parsed using Pandas dataframes. PDF's that are not text-based will not work with either of these solutions. This needs straight-up OCR. Will see if there exists some open-source package that can handle OCR parsing. Still not sure if this will all work, but it will help in automating the data extraction process. The next step will be to gather all the posted purchase receipts from NAV and seeing if I can perform matching from these to the extracted data fields. That will be really super interesting if this can be done. Maybe, if the data extraction is successful, I can incorporate machine learning from the python record linkage package in order to train for parameters pertaining to matching these transactions. That way, I'll have a model that can predict on which PR#'s a given data extraction should be belong to. We'll see.

--- test_script.py
+++ test_script.py
@@ -1,15 +1,30 @@
 from ctypes.util import find_library
 from ctypes import *
-import camelot
+import camelot, os
+import pandas as pd
+
+pd.set_option("display.max_columns", None)
+cwd = r"C:\Users\alext\Google Drive\weissscraper\static"
+print(cwd)
 
 print(find_library("".join(("gsdll", str(sizeof(c_voidp) * 8), ".dll"))))
 
-tables = camelot.read_pdf('test.PDF')
-print(tables)
+# tables = camelot.read_pdf('./static/test2.PDF', pages='all', flavor="stream", cols=5, edge_tol=500)
+# print(tables)
 
-tables.export('test.csv', f='csv', compress=True) # json, excel, html, sqlite
-tables[0]
-tables[0].parsing_report
-#
-tables[0].to_csv('test.csv') # to_json, to_excel, to_html, to_sqlite
+# tables.export('test2.csv', f='csv', compress=True) # json, excel, html, sqlite
+# print(tables[:])
+# [n for n in tables].parsing_report
+# #
+# tables[0].to_csv('test2.csv') # to_json, to_excel, to_html, to_sqlite
 # tables[0].df # get a pandas DataFrame!
+# for n in tables:
+#     print(n.df)
+
+invoice_file = 'DS20210309-1_INV.XLSX'
+
+output_file = os.path.join(cwd, invoice_file)
+
+invoice = pd.read_excel(output_file, sheet_name="INVOICE")
+
+print(invoice)
