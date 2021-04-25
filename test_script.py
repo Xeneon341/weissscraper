4fdee00b487c6c277c59012943282fe503c90275
Xeneon341 <56289476+Xeneon341@users.noreply.github.com>
2021-04-24 19:46:57 -0700

    was able to parse Word docx files for Liyang invoices. Since Liyang invoices, at least some of them, are in .doc format, I will need to convert these to .docx format for the parser to work, probably with pywin32 but not sure yet.

--- test_script.py
+++ test_script.py
@@ -1,6 +1,7 @@
 from ctypes.util import find_library
 from ctypes import *
-import camelot, os
+from docx.api import Document
+import camelot, os, docx2txt
 import pandas as pd
 
 pd.set_option("display.max_columns", None)
@@ -21,10 +22,33 @@ print(find_library("".join(("gsdll", str(sizeof(c_voidp) * 8), ".dll"))))
 # for n in tables:
 #     print(n.df)
 
-invoice_file = 'DS20210309-1_INV.XLSX'
+# invoice_file = 'DS20210309-1_INV.XLSX'
 
-output_file = os.path.join(cwd, invoice_file)
+# output_file = os.path.join(cwd, invoice_file)
 
-invoice = pd.read_excel(output_file, sheet_name="INVOICE")
+# invoice = pd.read_excel(output_file, sheet_name="INVOICE")
 
-print(invoice)
+# print(invoice)
+
+
+# document = Document('INVOICE--20HR5097.docx')
+# print(document)
+# table = document.tables[0]
+
+# data = []
+
+# keys = None
+# for i, row in enumerate(table.rows):
+#     text = (cell.text for cell in row.cells)
+
+#     if i == 0:
+#         keys = tuple(text)
+#         continue
+#     row_data = dict(zip(keys, text))
+#     data.append(row_data)
+#     print (data)
+
+# df = pd.DataFrame(data)
+
+my_text = docx2txt.process("INVOICE--20HR5097.docx")
+print(my_text)
